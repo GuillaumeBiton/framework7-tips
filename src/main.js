9348f3ae212964541c0ff186b9d636b22b34ea41
Guillaume Biton <guillaume.biton@free.fr>
2020-11-12 08:30:39 +0100

    :')

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 // Import Framework7
-import Framework7 from 'framework7-custom.js'
+import Framework7 from './framework7-custom'
 
 // Import Framework7-Svelte Plugin
 import Framework7Svelte from 'framework7-svelte';
 
 // Import Framework7 Styles
-import 'framework7-custom.less';
+import './framework7-custom.less';
 
 // Import custom App Styles
-import 'app.less';
+import './app.less';
 
 // Init F7 Svelte Plugin
 Framework7.use(Framework7Svelte)
 
-import App from 'App.svelte';
+import App from './App.svelte';
 
 var app = new App({
 	target: document.body
